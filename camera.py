edcc0ca40a7475fddcefdf5b3a6abd6f44bd51a3
Hariket Sukeshkumar Sheth <72455881+hariketsheth@users.noreply.github.com>
2021-05-06 16:28:51 +0530

    Rectified the cv2.HoughCircles parameters error

--- camera.py
+++ camera.py
@@ -10,8 +10,8 @@ class Video (object):
         self.camera.release()
  
     def main_exec(self):
-        green_L_hsv = np.array([45, 140, 50])
-        green_U_hsv = np.array([86, 255, 255])
+        green_L_hsv = np.array([39, 140, 50])
+        green_U_hsv = np.array([80, 255, 255])
         while True:
             response, frame = self.camera.read()
             if frame is None:
@@ -25,10 +25,10 @@ class Video (object):
             green_extract = cv.dilate(green_extract, None, iterations=2)
 
             
-            possible_balls = cv.HoughCircles(green_extract, cv.HOUGH_GRADIENT, 1, 25, param1=1, param2=45,minRadius=0, maxRadius=0)
+            possible_balls = cv.HoughCircles(green_extract, cv.HOUGH_GRADIENT, 1, 25, param1=2, param2=20,minRadius=0, maxRadius=0)
             if possible_balls is not None:
                 print("Green Ball Found !")
-                cv.putText(frame, "Green Ball Detected" ,org = tuple(possible_balls[0][0][:2]),color = (255, 255, 255))
+                cv.putText(frame, "Green Ball Detected" ,tuple(map(int,possible_balls[0][0][:2])),cv.FONT_HERSHEY_SIMPLEX,1,(255,255,255),3)
             else:
                 print("Green Ball Not Found !")
                 
@@ -53,8 +53,8 @@ class Video (object):
                 else:
                     print("NOT FOUND")
 
-            cv.imshow("window1",frame)
-            cv.imshow("window2", green_extract)
+            cv.imshow("The Actual Frame",frame)
+            cv.imshow("Extracted Green ball", green_extract)
             """
 
             if cv.waitKey(1) & 0xFF == ord('d'):
